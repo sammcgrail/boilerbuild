161fde955899e780badc645fead2a769bf0849c6
Sam McGrail <sammcgrail@gmail.com>
2022-05-03 21:35:09 -0400

    no more text

--- public/scripz/threeJs.js
+++ public/scripz/threeJs.js
@@ -1,5 +1,7 @@
 const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
 
 const makeACube = (material) => {
     const geometry = new THREE.BoxGeometry();
@@ -9,8 +11,6 @@ const makeACube = (material) => {
     return cube;
 }
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
