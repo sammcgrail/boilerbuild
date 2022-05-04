44baad31f5e9e39fd3ca08c15d76c19fa695b813
Sam McGrail <sammcgrail@gmail.com>
2022-05-03 21:32:48 -0400

    biblically accurate pizza that follows your mouse

--- public/scripz/threeJs.js
+++ public/scripz/threeJs.js
@@ -4,6 +4,7 @@ const loader = new THREE.TextureLoader();
 const makeACube = (material) => {
     const geometry = new THREE.BoxGeometry();
     const cube = new THREE.Mesh(geometry, material);
+    cube.maxFrames = Math.floor(Math.random() * 300);
     scene.add(cube);
     return cube;
 }
@@ -16,19 +17,55 @@ document.body.appendChild(renderer.domElement);
 const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const light = new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5);
 
+const curves = () => {
+    const curve = new THREE.QuadraticBezierCurve(
+        new THREE.Vector2(-2, -5),
+        new THREE.Vector2(-0.5, 2),
+        new THREE.Vector2(-2, 2),
+        new THREE.Vector2(2, 4),
+        new THREE.Vector2(2, 3)
+    );
+    const points = curve.getPoints(100);
+    return points;
+}
+
+const line = curves();
+let mouseX;
+let mouseY;
+let vec = new THREE.Vector3();
+let pos = new THREE.Vector3();
+document.onmousemove = (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    vec.set(
+        (mouseX / window.innerWidth) * 2 - 1,
+        -(mouseY / window.innerHeight) * 2 + 1,
+        0.5);
+    vec.unproject(camera);
+    vec.sub(camera.position).normalize();
+    const distance = -camera.position.z / vec.z;
+    pos.copy(camera.position).add(vec.multiplyScalar(distance));
+}
+
 let frames = 0;
 const animate = (cubes) => {
     requestAnimationFrame(() => animate(cubes));
     frames++;
-    cubes.forEach(cube => {
+    cubes.forEach((cube, i) => {
         cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
-        if(frames % 30 === 0){
-            cube.position.x = Math.random() * 10;
-            cube.position.y = Math.random() * 10;
-        }
+        // if(frames % cube.maxFrames === 0){
+        //     cube.position.x = -5 + Math.random() * 10;
+        //     cube.position.y = -5 + Math.random() * 10;
+        // }
+        // cube.position.x = line[(frames + i) % line.length].x + line[i].x;
+        // cube.position.y = line[(frames + i) % line.length].y + line[i].y;
+        // cube.position.z = -i * 0.5 + line[i].x * line[i].y; neat z-view trick
+        cube.position.x = pos.x + Math.sin(frames + i);
+        cube.position.y = pos.y + Math.cos(frames + i); 
+        // cube.position.z = -i + 6; don't do this
     })
-    
+
     renderer.render(scene, camera);
 }
 
@@ -41,8 +78,8 @@ loader.load(
         const cubes = [];
         for (let i = 0; i < 100; i++) {
             const cube = makeACube(material);
-            cube.position.x = i * 0.05;
-            cube.position.y = i * 0.05;
+            cube.position.x = line[i].x;
+            cube.position.y = line[i].y;
             cubes.push(cube);
         }
         console.log(cubes, scene);
