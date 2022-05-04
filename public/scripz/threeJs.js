cae8ff73e4fd96fa7d1ff15d6c45a5f023744352
Sam McGrail <sammcgrail@gmail.com>
2022-05-03 21:56:30 -0400

    expand my pizza plz

--- public/scripz/threeJs.js
+++ public/scripz/threeJs.js
@@ -15,6 +15,23 @@ const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const light = new THREE.HemisphereLight(0xf6e86d, 0x404040, 0.5);
 scene.add(light);
 
+let expansion = 0;
+document.onmousedown = () => {
+    expansion += 100;
+}
+const up = 38;
+const down = 40;
+document.onkeyup = (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode === up) {
+        expansion += 100;
+    } else if (event.keyCode === down) {
+        if (expansion > 100) {
+            expansion -= 100;
+        }
+    }
+}
+
 document.body.appendChild(renderer.domElement);
 
 document.onmousemove = (event) => {
@@ -29,6 +46,8 @@ document.onmousemove = (event) => {
     const distance = -camera.position.z / vec.z;
     pos.copy(camera.position).add(vec.multiplyScalar(distance));
 }
+
+
 
 const makeACube = (material) => {
     const geometry = new THREE.BoxGeometry();
@@ -69,6 +88,9 @@ const line = curves();
 let frames = 0;
 const animate = (cubes) => {
     requestAnimationFrame(() => animate(cubes));
+    if (expansion > 0) {
+        expansion--;
+    }
     frames++;
     cubes.forEach((cube, i) => {
         cube.rotation.x = Math.sin(frames + i)
@@ -80,8 +102,8 @@ const animate = (cubes) => {
         // cube.position.x = line[(frames + i) % line.length].x + line[i].x;
         // cube.position.y = line[(frames + i) % line.length].y + line[i].y;
         // cube.position.z = -i * 0.5 + line[i].x * line[i].y; neat z-view trick
-        cube.position.x = pos.x + Math.sin(frames + i);
-        cube.position.y = pos.y + Math.cos(frames + i);
+        cube.position.x = pos.x + Math.sin(frames + i) * (expansion / 100);
+        cube.position.y = pos.y + Math.cos(frames + i) * (expansion / 100);
         // cube.position.z = -i + 6; don't do this
     })
 
