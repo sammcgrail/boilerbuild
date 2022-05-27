82d291412bc25d2c45a216d6b56d589c30ac0ebe
Sam McGrail <sammcgrail@gmail.com>
2022-05-27 11:16:55 -0400

    just leave camera pos

--- public/scripz/mydatgui.js
+++ public/scripz/mydatgui.js
@@ -1,8 +1,14 @@
 var gui = new dat.GUI({name: 'Pizza Config'});
-var folder1 = gui.addFolder('Pizza Size');
-var size = {size: 50};
-gui.add(size, 'size', 0, 100);
-var folder2 = gui.addFolder('Room Lighting');
+// var folder1 = gui.addFolder('Pizza Size');
+// var size = {size: 50};
+// gui.add(size, 'size', 0, 100);
+// var folder2 = gui.addFolder('Room Lighting');
+// folder2.open(); 
 
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 100)
+const cameraFolder = gui.addFolder('Camera');   
+// cameraFolder.add(camera.position, 'x', 0, 100);
+// cameraFolder.add(camera.position, 'y', 0, 100);
+cameraFolder.add(camera.position, 'z', 0, 100);
+cameraFolder.open();
+// const expansionXslider = gui.addFolder('expando')
+// expansionXslider.add(expansionX, 0, 100)
