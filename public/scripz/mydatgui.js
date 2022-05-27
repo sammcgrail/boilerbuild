bf953ec6f803194d95898ec44ae93bb7bffd6837
Sam McGrail <sammcgrail@gmail.com>
2022-05-27 11:08:28 -0400

    add cam controls

--- public/scripz/mydatgui.js
+++ public/scripz/mydatgui.js
@@ -3,3 +3,6 @@ var folder1 = gui.addFolder('Pizza Size');
 var size = {size: 50};
 gui.add(size, 'size', 0, 100);
 var folder2 = gui.addFolder('Room Lighting');
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 100)
